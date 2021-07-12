c79a3a13f72e8566fd88469d4110190ee9dbb067
Bhardwaj Avasarala <bhardwaj@avb.dev>
2021-07-12 13:14:27 -0700

    Added technologies component

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
-);
+const Technologies: React.FC = () => {
+  return (
+    <Section id="tech">
+      <SectionDivider />
+      <SectionTitle style={{ paddingTop: "4rem" }}>Technologies</SectionTitle>
+      <SectionText>
+        I have worked on various projects exploring different technologies in
+        Frontend and Backend.
+      </SectionText>
+      <List>
+        <ListItem>
+          <DiReact size="3rem" />
+          <ListContainer>
+            <ListTitle>Frontend</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              React.js, Typescript, Node.js
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+
+        <ListItem>
+          <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListTitle>Backend</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              .NET, python, Node.js
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+
+        <ListItem>
+          <DiZend size="3rem" />
+          <ListContainer>
+            <ListTitle>UI/UX</ListTitle>
+            <ListParagraph>
+              Experience with tools like <br />
+              Figma, Adobe Photoshop, Adobe AfterEffects
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
+  );
+};
 
 export default Technologies;
