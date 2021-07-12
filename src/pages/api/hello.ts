e421f4cc95c1c02eca33db96a23e9ef85186599c
Bhardwaj Avasarala <bhardwaj@avb.dev>
2021-07-11 20:08:24 -0700

    Update File names

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+export default (req:any, res:any) => {
   res.status(200).json({ name: 'John Doe' })
 }
