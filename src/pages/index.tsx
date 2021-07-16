7e595893b80392e4c86036f2a19c96237c6cf076
Bhardwaj Avasarala <bhardwaj@avb.dev>
2021-07-15 21:35:48 -0700

    Revert "Update discord integration"
    This reverts commit 1a6228590b4d3ad9b1e52d4addaa2010e85b75d6.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,32 +10,14 @@ import { Section } from "../styles/GlobalComponents";
 const Home = () => {
   return (
     <Layout>
-      {/* <Section grid>
+      <Section grid>
         <Hero />
         <BgAnimation />
-      </Section> */}
+      </Section>
       {/* <Projects /> */}
-      {/* <Technologies />
+      <Technologies />
       <Timeline />
-      <Accomplishments /> */}
-      {/* <iframe
-        title="React-i-flux"
-        src="https://discord.com/channels/102860784329052160/377580704722190347"
-      /> */}
-      <iframe
-        title="discord channel"
-        src="http://discord.com/widget?id=865431716839030844&theme=dark"
-        width="600"
-        height="600"
-        // allowtransparency="true"
-        // frameborder="0"
-        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-      ></iframe>
-
-      {/* <iframe
-        src="https://discord.com/api/webhooks/865436437134049301/GdVnQiXUU2MpgLrBH5YINrgfz4mDBCopt_V3ya0v1jYnPEayuUArMLjCIbtZRTxAbvCI"
-        title="ra"
-      /> */}
+      <Accomplishments />
     </Layout>
   );
 };
